2bef54b8a54f34c8c40030990a3ad80022f0a9ec
Natalia Bujnowicz <nataliabujnowicz1987@gmail.com>
2020-08-12 14:28:49 +0100

    Updates

---
+++ precache-manifest.20ef87c1bbb175d53eef505ec3bce287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc48425ea2ae0cc18718a58dfe00c7b8",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "48cdcef4f4db89ee85a9",
+    "url": "/Recipe-App/static/css/main.b20501cb.chunk.css"
+  },
+  {
+    "revision": "f6683532279832adc15f",
+    "url": "/Recipe-App/static/js/2.a8ee570a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.a8ee570a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48cdcef4f4db89ee85a9",
+    "url": "/Recipe-App/static/js/main.24945f76.chunk.js"
+  },
+  {
+    "revision": "90045ec2e2b4c21230b6",
+    "url": "/Recipe-App/static/js/runtime-main.7a18362e.js"
+  }
+]);
